9b52fd18cc02d74211339701c644535949f703a4
Grupo Colraices <juancamilo.rivera@agenciamomentum.co>
2026-07-06 02:44:37 -0500

    feat(auth): instalar Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,20 @@
 <?php
 
-use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/clientes', [ClienteController::class, 'index'])
-    ->name('clientes.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/clientes/create', [ClienteController::class, 'create'])
-    ->name('clientes.create');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::post('/clientes', [ClienteController::class, 'store'])
-    ->name('clientes.store');
-
-Route::get('/clientes/{cliente}', [ClienteController::class, 'show'])
-    ->name('clientes.show');
-
-Route::get('/clientes/{cliente}/edit', [ClienteController::class, 'edit'])
-    ->name('clientes.edit');
-
-Route::put('/clientes/{cliente}', [ClienteController::class, 'update'])
-    ->name('clientes.update');
-
-Route::delete('/clientes/{cliente}', [ClienteController::class, 'destroy'])
-    ->name('clientes.destroy');
+require __DIR__.'/auth.php';
